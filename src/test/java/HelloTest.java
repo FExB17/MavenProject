21868e173f65f942550d191c6d3d4003ce53c826
Furkan Ervan <furkanervan@hotmail.de>
2022-12-04 19:19:58 +0100

    some more work

--- src/test/java/HelloTest.java
+++ src/test/java/HelloTest.java
@@ -2,6 +2,9 @@ public class HelloTest {
     public static void main(String[] args) {
 
         System.out.println("Hello Test");
+        System.out.println("Testing some more");
+
+        System.out.println("Github is published");
 
     }
 }
